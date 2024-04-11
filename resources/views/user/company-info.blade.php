13acc932a1834c03140012cbee9752bf6296b928
Dat <datvoanh95@gmail.com>
2024-04-11 22:11:58 +0700

    add page company info

---
+++ resources/views/user/company-info.blade.php
@@ -0,0 +1,29 @@
+@extends('user.layout')
+
+@section('content')
+	<div id="right_ok">
+		<div class="caption">THÔNG TIN CÔNG TY</div>
+		<div class="caption_en">Company infomation</div>
+		
+		{{-- <div class="m-t-40"></div>
+
+		<p><strong>CÔNG TY TNHH XUẤT NHẬP KHẨU & TM HTS</strong> cung cấp và cho thuê nhiều loại sản phẩm 
+			<br>với cơ sở vật chất tiêu chuẩn của các thương hiệu hàng đầu.</p>
+
+	    <div class="m-t-20"></div>
+
+	    <p><strong>HTS IMPORT EXPORT & TRADING COMPANY LIMITED</strong> supplies and rents a variety of 
+	    	<br>products with standard facilities of leading brands.</p>
+
+	    <div class="m-t-20"></div>
+
+	    <div class="box-images">
+			<div class="image-left">
+	            <img src="{{ asset('images/kobelco.png') }}" alt="">
+	        </div>
+	        <div class="image-right">
+	        	<img src="{{ asset('images/hyundai.png') }}" alt="">
+	        </div>
+		</div> --}}
+	</div>
+@endsection
