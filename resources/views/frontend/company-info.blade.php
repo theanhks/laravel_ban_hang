b64af0ba9dd33ca723c0664e4f6d62f7887d1ba9
Dat <datvoanh95@gmail.com>
2024-05-07 22:50:11 +0700

    update page company-information

--- resources/views/frontend/company-info.blade.php
+++ resources/views/frontend/company-info.blade.php
@@ -2,28 +2,19 @@
 
 @section('content')
 	<div id="right_ok">
-		<div class="caption white-color">THÔNG TIN CÔNG TY</div>
-		<div class="caption_en white-color">Company infomation</div>
+		<div class="caption">THÔNG TIN CÔNG TY</div>  {{-- white-color --}}
+		<div class="caption_en">Company infomation</div>
 
-		{{-- <div class="m-t-40"></div>
+		<div class="m-t-40"></div>
 
-		<p><strong>CÔNG TY TNHH XUẤT NHẬP KHẨU & TM HTS</strong> cung cấp và cho thuê nhiều loại sản phẩm
-			<br>với cơ sở vật chất tiêu chuẩn của các thương hiệu hàng đầu.</p>
-
-	    <div class="m-t-20"></div>
-
-	    <p><strong>HTS IMPORT EXPORT & TRADING COMPANY LIMITED</strong> supplies and rents a variety of
-	    	<br>products with standard facilities of leading brands.</p>
-
-	    <div class="m-t-20"></div>
-
-	    <div class="box-images">
-			<div class="image-left">
-	            <img src="{{ asset('images/kobelco.png') }}" alt="">
+		<div class="box-image-license">
+			<div class="img-bus img-left">
+	            <img src="{{ asset('images/license-business-1.png') }}" alt="">
 	        </div>
-	        <div class="image-right">
-	        	<img src="{{ asset('images/hyundai.png') }}" alt="">
+	        <div class="img-bus img-right">
+	        	<img src="{{ asset('images/license-business-2.png') }}" alt="">
 	        </div>
-		</div> --}}
+		</div>
 	</div>
 @endsection
+
