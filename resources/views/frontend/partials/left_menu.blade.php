7fca735965394efd4b7478729c96ce830ca0a605
The Anh <trandotheanh@gmail.com>
2024-05-13 20:15:58 +0700

    add prodcut

--- resources/views/frontend/partials/left_menu.blade.php
+++ resources/views/frontend/partials/left_menu.blade.php
@@ -34,12 +34,12 @@
                 <div class="hotline_l">07 74 74 74 33</div>
                 <div class="item_ht">
                     <p class="phong">Nhân Viên Kinh Doanh</p>
-                    <p><i class="fas fa-phone-volume"></i>0976 400 078(Nhân viên kinh doanh 1)</p>
+                    <p><i class="fas fa-phone-volume"></i>0914.860.432 (Nhân viên kinh doanh)</p>
                     <p><i class="fas fa-envelope"></i>htsvn.com.vn</p>
                 </div>
                 <div class="item_ht">
                     <p class="phong">Nhân Viên Kĩ Thuật</p>
-                    <p><i class="fas fa-phone-volume"></i>0976 400 078(Mr Kĩ Thuật 1)</p>
+                    <p><i class="fas fa-phone-volume"></i>0914.860.432(Mr Kĩ Thuật)</p>
                     <p><i class="fas fa-envelope"></i>htsvn.com.vn</p>
                 </div>
                 <div class="clear"></div>
